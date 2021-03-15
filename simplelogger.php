edd1d77352f5d919af9c636b1844e4c67e25d73e
x183 <55193662+x183@users.noreply.github.com>
2021-03-15 18:28:49 +0100

    Update simplelogger.php

--- simplelogger.php
+++ simplelogger.php
@@ -1,8 +1,13 @@
 <?php
 
 $ip = $_SERVER["REMOTE_ADDR"];
-$f=fopen("text.txt","a")or die();
-fwrite($f,"$ip\n");
+$time=date("Y-m-d (H:i:s)");
+$browser=$_SERVER["HTTP_USER_AGENT"];
+
+$out="{\n\t$time\n\t$ip\n\t$browser\n}";
+$f=fopen(".secret/text.txt","a")or die();
+fwrite($f,"$out\n");
+
 fclose($f);
 
 ?>
